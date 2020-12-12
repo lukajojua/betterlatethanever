83c3529d960d65a897b4333d88e176c2aad1bef4
lukajojua <73716112+lukajojua@users.noreply.github.com>
2020-12-12 07:22:18 +0400

    first commit

---
+++ app/src/main/java/com/example/fragment/Fragments/IDK.kt
@@ -0,0 +1,13 @@
+package com.example.fragment.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragment.R
+
+
+class IDK : Fragment(R.layout.fragment_i_d_k) {
+
+}
